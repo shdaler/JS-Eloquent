d34242c9c987a9b696134fb00a150e85e314b8a1
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-02-24 11:25:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,16 @@
 //   console.log(line);
 // }
 
-for (let i = 1; i < 7; i++) {
-  let line = "#".repeat(i);
-  console.log(line);
+// for (let i = 1; i < 7; i++) {
+//   let line = "#".repeat(i);
+//   console.log(line);
+// }
+
+for (let i = 1; i <= 100; i++) {
+  let output = "";
+
+  if (i % 3 === 0) output += "Fizz";
+  if (i % 5 === 0) output += "Buzz";
+
+  console.log(output || i);
 }
