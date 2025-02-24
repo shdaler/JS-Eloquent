fa0fe6d65c5d9600f5f11b1da9e31436ddb1a08f
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-02-24 11:23:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,11 @@
 //   console.log(line);
 // }
 
-for (let line = "#"; line.length < 8; line += "#") {
+// for (let line = "#"; line.length < 8; line += "#") {
+//   console.log(line);
+// }
+
+for (let i = 1; i < 7; i++) {
+  let line = "#".repeat(i);
   console.log(line);
 }
