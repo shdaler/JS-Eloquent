22a4f69c860cd1280d4d1c3e2fa1ef9733412380
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-03-12 10:19:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -52,8 +52,16 @@
 //   console.log("Toot");
 // };
 
-function greet(who) {
-  console.log("Hello " + who);
+// function greet(who) {
+//   console.log("Hello " + who);
+// }
+// greet("Harry");
+// console.log("Bye");
+
+function chicken() {
+  return egg();
 }
-greet("Harry");
-console.log("Bye");
+function egg() {
+  return chicken();
+}
+console.log(chicken() + " came first.");
