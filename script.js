26438b910de09aa0d482a195ec247ebf811e297b
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2025-03-21 22:25:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -122,12 +122,30 @@
 // console.log(counter.decrement()); // 1
 // console.log(counter.getCount()); // 1
 
-function power(base, exponent) {
-  if (exponent == 0) {
-    return 1;
-  } else {
-    return base * power(base, exponent - 1);
+// function power(base, exponent) {
+//   if (exponent == 0) {
+//     return 1;
+//   } else {
+//     return base * power(base, exponent - 1);
+//   }
+// }
+
+// console.log(power(2, 3));
+
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) {
+      return history;
+    } else if (current > target) {
+      return null;
+    } else {
+      return (
+        find(current + 5, `(${history} + 5)`) ??
+        find(current * 3, `(${history} * 3)`)
+      );
+    }
   }
+  return find(1, "1");
 }
 
-console.log(power(2, 3));
+console.log(findSolution(24));
